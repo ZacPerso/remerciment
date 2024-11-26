ea5057d8f0050e5b098b047573c86b678b4a67fd
Zac <122275194+ZacPerso@users.noreply.github.com>
2024-11-26 20:29:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function scrollToVideo() {
 
     // Lance la vidéo automatiquement
     const video = document.querySelector('video');
+    wordflick();
     video.play();
 }
 
@@ -21,3 +22,50 @@ window.addEventListener("load", () => {
         window.scrollTo(0, 0); // Revenir en haut après le chargement
     }, 0);
 });
+
+
+
+var
+  words = ['Baraka Allahu fiki','Merci beaucoup','Tu es la meilleure'],
+  part,
+  i = 0,
+  offset = 0,
+  len = words.length,
+  forwards = true,
+  skip_count = 0,
+  skip_delay = 5,
+  speed = 100;
+
+var wordflick = function(){
+  setInterval(function(){
+      if (forwards) {
+        if(offset >= words[i].length){
+          ++skip_count;
+          if (skip_count == skip_delay) {
+            forwards = false;
+            skip_count = 0;
+          }
+        }
+      }
+      else {
+         if(offset == 0){
+            forwards = true;
+            i++;
+            offset = 0;
+            if(i >= len){
+              i=0;
+            } 
+         }
+      }
+      part = words[i].substr(0, offset);
+      if (skip_count == 0) {
+        if (forwards) {
+          offset++;
+        }
+        else {
+          offset--;
+        }
+      }
+    	$('.word').text(part);
+  },speed);
+};
